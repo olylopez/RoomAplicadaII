549379c3e0787cbd0514085120804419d26166a3
Oly Lopez Saldivar <oly_lopez@ucne.edu.do>
2024-05-26 15:41:02 -0400

    Nav Completo, falta con el boton nuevo. solo agrega el id 0(cero)

--- app/src/main/java/edu/ucne/roomaplicadaii/presentation/tecnico/TecnicoListScreen.kt
+++ app/src/main/java/edu/ucne/roomaplicadaii/presentation/tecnico/TecnicoListScreen.kt
@@ -1,12 +1,11 @@
 package edu.ucne.roomaplicadaii.presentation.tecnico
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,6 +13,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,6 +24,7 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -97,76 +98,77 @@ fun TecnicoListBory(
                     Text(
                         text = "Id",
                         style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
-                        modifier = Modifier.weight(0.5f)
+                        modifier = Modifier.weight(1f)
                     )
-
                     Text(
                         text = "Nombres",
                         style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
-                        modifier = Modifier.weight(2.5f)
+                        modifier = Modifier.weight(3f)
                     )
-
                     Text(
                         text = "Sueldo por Hora",
                         style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
-                        modifier = Modifier.weight(1.5f)
-                    )
-
-                    Text(
-                        text = "tipo",
-                        style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
                         modifier = Modifier.weight(2f)
                     )
-                    Spacer(modifier = Modifier.weight(0.8f))
-                    Spacer(modifier = Modifier.weight(0.8f))
+                    Text(
+                        text = "Tipo",
+                        style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+                        modifier = Modifier.weight(1f)
+                    )
                 }
+                Divider(color = Color.Gray, thickness = 1.dp, modifier = Modifier.padding(vertical = 4.dp))
             }
 
             LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
+                modifier = Modifier.fillMaxSize()
             ) {
                 items(tecnicos) { tecnico ->
-                    Row(
+                    Column(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(16.dp)
+                            .padding(8.dp)
                             .background(
                                 color = Color.LightGray,
-                                shape = RoundedCornerShape(1.dp)
+                                shape = RoundedCornerShape(4.dp)
                             )
                     ) {
-                        Text(
-                            text = tecnico.tecnicoId.toString(),
-                            modifier = Modifier.weight(0.5f)
-                        )
-                        Text(text = tecnico.nombres.toString(), modifier = Modifier.weight(2.5f))
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(
-                            text = "$" + tecnico.sueldoHora.toString(),
-                            modifier = Modifier.weight(1.5f)
-                        )
-                        Text(text = tecnico.tipo.toString(), modifier = Modifier.weight(2f))
-                        IconButton(
-                            onClick = { onDeleteTecnido(tecnico) },
-                            modifier = Modifier.weight(0.8f),
-                            content = {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                        ) {
+                            Text(text = tecnico.tecnicoId.toString(), modifier = Modifier.weight(1f))
+                            tecnico.nombres?.let { Text(text = it, modifier = Modifier.weight(3f)) }
+                            Text(text = "$${tecnico.sueldoHora}", modifier = Modifier.weight(2f))
+                            tecnico.tipo?.let { Text(text = it, modifier = Modifier.weight(1f)) }
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            IconButton(
+                                onClick = { onDeleteTecnido(tecnico) },
+                                modifier = Modifier.padding(horizontal = 8.dp)
+                            ) {
                                 Icon(
                                     imageVector = Icons.Default.Delete,
                                     contentDescription = "delete button"
                                 )
                             }
-                        )
-                        IconButton(
-                            onClick = { onVerTecnico(tecnico) },
-                            modifier = Modifier.weight(0.8f),
-                            content = {
+                            IconButton(
+                                onClick = { onVerTecnico(tecnico) },
+                                modifier = Modifier.padding(horizontal = 8.dp)
+                            ) {
                                 Icon(
                                     imageVector = Icons.Default.Edit,
                                     contentDescription = "edit button"
                                 )
                             }
-                        )
+                        }
                     }
                 }
             }
